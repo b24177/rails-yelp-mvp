bf4c914b789893e3fad798b072d92c0dcc67bf8e
Brant Monaghan <monaghanb@gmail.com>
2020-08-18 21:03:15 +0200

    Some more validations done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, presence: true
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
   validates :address, presence: true
   validates :category, presence: true
 end
